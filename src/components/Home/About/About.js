8d026c3cef811d56fb0795d829eb87e107f2ffb8
Anhar-Hossain-Khan <anhar.hossain1997@gmail.com>
2021-10-27 15:58:40 +0600

    final update

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -9,12 +9,12 @@ const About = () => {
           <div class="info-top">
             <h2>Why are you come with us?</h2>
           </div>
-          <Row lg={12} className="info-row">
-            <Col lg={6} className="info-description">
+          <Row sm={12} lg={12} className="info-row">
+            <Col sm={6}  lg={6} className="info-description">
               <h3>Find Your Dream Home By Us</h3>
-              <h6>We are committed to providing exceptional services, every <br />details and true support from the start of your home transaction until the end. </h6>
+              <h6>We are committed to providing exceptional services, every <br />details and true support from the start of your home transaction until <br /> the end. </h6>
             </Col>
-            <Col lg={6} >
+            <Col sm={6}  lg={6} >
               <img className="border border-primary rounded-3 w-100 h-75 " src={property2} alt="" />
             </Col>
           </Row>
